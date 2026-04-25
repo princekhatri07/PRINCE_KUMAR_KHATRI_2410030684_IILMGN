d224edc4bc6ad24a39a3a10948589bca557ea0fe
PRINCE KHATRI <princekumarkhatri07@gmail.com>
2026-04-25 09:36:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        arr.reverse()
+        for i in range(len(arr),0):
+            print(arr[i])
+    
+        
+       
+            
+            
+        
+            
+        
+        
+        
+        
+    
+        
+        
+        
